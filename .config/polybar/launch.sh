0ac2bdbe4d7cbfa60cbae0723ba77c791aacce09
Keaton Burleson <keaton.burleson@fedex.com>
2021-09-08 13:54:09 -0500

    Adds Polybar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,12 @@
+# etc/bin/zsh
+
+sleep .5
+
+if ! pgrep -x polybar; then
+	polybar -c ~/.config/polybar/config -q bottom &  polybar -c ~/.config/polybar/config -q top &
+else
+	# Kill prev polybar session
+	pkill -USR1 polybar
+fi
+
+echo "Bar launched..."
